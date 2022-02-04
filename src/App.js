b45d62fa71c35cffcad0c8de84fa9fd101638ed9
baran beyazıt <byztmain2121@gmail.com>
2022-02-04 16:30:13 +0300

    tailwind setup is finished

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+  <h1 className="text-3xl font-bold underline bg-violet-500 text-white">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
